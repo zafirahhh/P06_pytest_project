0ef7bd4bcfeacfff21e04c99fdd9de71847c76ea
RP+23049844@DESKTOP-1L5IPT1 <23049844@myrp.edu.sg>
2024-05-21 15:29:13 +0800

    initial commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,54 @@
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_subtract(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.subtract(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+    
+    def test_multiply(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.multiply(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_divide(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.divide(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
